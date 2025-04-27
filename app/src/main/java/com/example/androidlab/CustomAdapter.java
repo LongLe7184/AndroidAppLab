5a7bc67fea74323f40b69b772ee5f128694bb2e0
LongLe7184 <lengoclong2003@gmail.com>
2025-04-27 13:17:19 +0700

    April-27th Ex5.1

---
+++ app/src/main/java/com/example/androidlab/CustomAdapter.java
@@ -0,0 +1,55 @@
+package com.example.androidlab;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.BaseAdapter;
+
+public class CustomAdapter extends BaseAdapter {
+    private Context context;
+    private String[] labels;
+    private String[] descriptions;
+    private int[] icons;
+
+    public CustomAdapter(Context context, String[] labels, String[] descriptions, int[] icons) {
+        this.context = context;
+        this.labels = labels;
+        this.descriptions = descriptions;
+        this.icons = icons;
+    }
+
+    @Override
+    public int getCount() {
+        return labels.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            LayoutInflater inflater = LayoutInflater.from(context);
+            convertView = inflater.inflate(R.layout.item_list, parent, false);
+        }
+
+        ImageView icon = convertView.findViewById(R.id.item_icon);
+        TextView label = convertView.findViewById(R.id.item_label);
+        TextView description = convertView.findViewById(R.id.item_description);
+
+        icon.setImageResource(icons[position]);
+        label.setText(labels[position]);
+        description.setText(descriptions[position]);
+
+        return convertView;
+    }
+}
--- app/src/main/java/com/example/androidlab/MainActivity.java
+++ app/src/main/java/com/example/androidlab/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.androidlab;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +21,22 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        ListView listView = findViewById(R.id.myListView);
+
+        int[] icons = {R.drawable.sticker1, R.drawable.sticker2, R.drawable.sticker3,
+                       R.drawable.sticker4, R.drawable.sticker5, R.drawable.sticker6,
+                       R.drawable.sticker7, R.drawable.sticker8, R.drawable.sticker9};
+
+        String[] labels = {"Shy", "Happy", "Amazing",
+                           "Surprise", "Angry", "Pensive",
+                           "Sleep", "Cool", "Think"};
+
+        String[] descriptions = {"Shy Emote sticker", "Happy Emote sticker", "Amazing Emote sticker",
+                                 "Surprise Emote sticker", "Angry Emote sticker", "Pensive Emote sticker",
+                                 "Sleep Emote sticker", "Cool Emote sticker", "Think Emote sticker"};
+
+        CustomAdapter adapter = new CustomAdapter(this, labels, descriptions, icons);
+        listView.setAdapter(adapter);
     }
 }
