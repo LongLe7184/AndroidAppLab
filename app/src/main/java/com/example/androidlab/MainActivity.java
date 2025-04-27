69ee35638b19650df7d7aa1f9c54002a6450b042
LongLe7184 <lengoclong2003@gmail.com>
2025-04-27 13:35:04 +0700

    April-27th Ex5.2

--- app/src/main/java/com/example/androidlab/MainActivity.java
+++ app/src/main/java/com/example/androidlab/MainActivity.java
@@ -24,17 +24,17 @@ public class MainActivity extends AppCompatActivity {
 
         ListView listView = findViewById(R.id.myListView);
 
-        int[] icons = {R.drawable.sticker1, R.drawable.sticker2, R.drawable.sticker3,
-                       R.drawable.sticker4, R.drawable.sticker5, R.drawable.sticker6,
-                       R.drawable.sticker7, R.drawable.sticker8, R.drawable.sticker9};
+        int[] icons = {R.drawable.apple, R.drawable.avocado, R.drawable.banana,
+                       R.drawable.blueberry, R.drawable.cherry, R.drawable.dragonfruit,
+                       R.drawable.pear, R.drawable.pineapple, R.drawable.watermelon};
 
-        String[] labels = {"Shy", "Happy", "Amazing",
-                           "Surprise", "Angry", "Pensive",
-                           "Sleep", "Cool", "Think"};
+        String[] labels = {"Apple", "Avocado", "Banana",
+                           "Blueberry", "Cherry", "Dragon Fruit",
+                           "Pear", "Pineapple", "Watermelon"};
 
-        String[] descriptions = {"Shy Emote sticker", "Happy Emote sticker", "Amazing Emote sticker",
-                                 "Surprise Emote sticker", "Angry Emote sticker", "Pensive Emote sticker",
-                                 "Sleep Emote sticker", "Cool Emote sticker", "Think Emote sticker"};
+        String[] descriptions = {"50 Calories", "97 Calories", "82 Calories",
+                                 "64 Calories", "33 Calories", "47 Calories",
+                                 "52 Calories", "48 Calories", "76 Calories",};
 
         CustomAdapter adapter = new CustomAdapter(this, labels, descriptions, icons);
         listView.setAdapter(adapter);
